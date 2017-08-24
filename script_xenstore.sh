936585cd03b0ceb1011dbe05cb4889ad8fc7e3b8
amnawaseem <amna.waqar1@yahoo.com>
2017-08-24 16:53:20 +0200

    Create script_xenstore.sh

---
+++ script_xenstore.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd /usr/local/bin/
+chmod +x *
+
+./xenstore-write /local/domain/0/domid 0
+./xenstore-write /local/domain/0/name Domain-0
+./xenstore-write /local/domain/0/control/shutdown ""
+./xenstore-write /local/domain/0/control/feature-poweroff 0
+./xenstore-write /local/domain/0/control/feature-halt 0
+./xenstore-write /local/domain/0/control/feature-suspend 0
+./xenstore-write /local/domain/0/control/feature-reboot 0
+./xenstore-write /local/domain/1/device ""
+./xenstore-chmod /local/domain/1/device rw
+./xenstore-write /local/domain/1/device/vif ""
+./xenstore-chmod /local/domain/1/device/vif rw
+./xenstore-write /local/domain/1/device/vif/0 ""
+./xenstore-chmod /local/domain/1/device/vif/0 rw
+./xenstore-write /local/domain/1/device/vif/0/backend-id 0
+./xenstore-chmod /local/domain/1/device/vif/0/backend-id r
+./xenstore-write /local/domain/1/device/vif/0/mac "D2:A3:CD:27:4A:53"
+./xenstore-chmod /local/domain/1/device/vif/0/mac rw
+./xenstore-write /local/domain/1/device/vif/0/backend "/local/domain/0/backend/vif/1/0"
+./xenstore-chmod /local/domain/1/device/vif/0/backend rw
+./xenstore-write /local/domain/0/backend/vif/1/0/frontend-id 1
+./xenstore-write /local/domain/0/backend/vif/1/0/frontend "/local/domain/1/device/vif/0"
+./xenstore-write /local/domain/0/backend/vif/1/0/script "/etc/xen/scripts/vif-route"
+./xenstore-write /local/domain/0/backend/vif/1/0/handle 0
+./xenstore-chmod /local/domain/0/backend/vif/1/0 rw
+./xenstore-write /local/domain/1/device/vif/0/state 1
+./xenstore-write /local/domain/0/backend/vif/1/0/state 1
